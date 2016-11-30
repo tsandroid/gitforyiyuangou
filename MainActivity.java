9a1d74ba1216488db814fbfa34589c0c4414cc4d
Sen tian <13813447429@163.com>
2016-11-30 10:58:52 +0800

    add again

--- MainActivity.java
+++ MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends FragmentActivity {
 	}
 	
 	
-	//git test
+	//git test again
 	private View getTabItemView(int index) {
 		View view = View.inflate(this, R.layout.maintab_item, null);
 		ImageView ivIcon = (ImageView) view.findViewById(R.id.iv_maintab_icon);
